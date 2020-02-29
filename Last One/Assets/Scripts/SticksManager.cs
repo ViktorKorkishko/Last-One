7bd3a3fd4d36906046304cf9edc3a3f91d0a24b4
ViktorKorkishko <ViktorKorkishko@users.noreply.github.com>
2020-02-29 15:02:13 +0200

    Finishing Game Mechanics

--- Last One/Assets/Scripts/SticksManager.cs
+++ Last One/Assets/Scripts/SticksManager.cs
@@ -10,12 +10,11 @@ public class SticksManager : MonoBehaviour
 
     public void UpdateAvailableSticks()
     {
-        for (int i = 0; i < sticks.Count; i++)
+        for (int i = 3; i < sticks.Count; i++)
         {
             if (i < lastAvailableStick)
             {
-                sticks[i - 1].GetComponent<BoxCollider2D>().enabled = true;
-                Debug.Log("Stick #" + (i - 1).ToString() + " switched on!");
+                sticks[i].GetComponent<BoxCollider2D>().enabled = true;
             }
         }
     }
